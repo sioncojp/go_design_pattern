4e1b0ccb1ff7387a4548385e8735d42de0306a36
sioncojp <sion_cojp@yahoo.co.jp>
2021-10-20 19:17:53 +0900

    implement factory method pattern part2

--- factory_method/1/factory_method.go
+++ factory_method/1/factory_method.go
@@ -1,4 +1,4 @@
-package factory_method
+package _
 
 import (
 	"fmt"
---
+++ factory_method/2/factory_method.go
@@ -0,0 +1,81 @@
+package main
+
+import "fmt"
+
+type iGun interface {
+	setName(name string)
+	setPower(power int)
+	getName() string
+	getPower() int
+}
+
+type ak47 struct {
+	gun
+}
+type musket struct {
+	gun
+}
+
+func newAk47() iGun {
+	return &ak47{
+		gun: gun{
+			name:  "AK47 gun",
+			power: 4,
+		},
+	}
+}
+
+func newMusket() iGun {
+	return &musket{
+		gun: gun{
+			name:  "Musket gun",
+			power: 1,
+		},
+	}
+}
+
+type gun struct {
+	name  string
+	power int
+}
+
+func (g *gun) setName(name string) {
+	g.name = name
+}
+
+func (g *gun) getName() string {
+	return g.name
+}
+
+func (g *gun) setPower(power int) {
+	g.power = power
+}
+
+func (g *gun) getPower() int {
+	return g.power
+}
+
+func getGun(gunType string) (iGun, error) {
+	if gunType == "ak47" {
+		return newAk47(), nil
+	}
+	if gunType == "musket" {
+		return newMusket(), nil
+	}
+	return nil, fmt.Errorf("Wrong gun type passed")
+}
+
+func printDetails(g iGun) {
+	fmt.Printf("Gun: %s", g.getName())
+	fmt.Println()
+	fmt.Printf("Power: %d", g.getPower())
+	fmt.Println()
+}
+
+func main() {
+	ak47, _ := getGun("ak47")
+	musket, _ := getGun("musket")
+
+	printDetails(ak47)
+	printDetails(musket)
+}
