3fdfde965b485227f1edf7ef45500eb0911efbbe
sioncojp <sion_cojp@yahoo.co.jp>
2021-11-08 18:43:40 +0900

    add facade

---
+++ facade/facade.go
@@ -0,0 +1,21 @@
+package facade
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	walletFacade := newWalletFacade("abc", 1234)
+
+	err := walletFacade.addMoneyToWallet("abc", 1234, 10)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err.Error())
+	}
+
+	fmt.Println()
+	err = walletFacade.deductMoneyFromWallet("abc", 1234, 5)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err.Error())
+	}
+}
