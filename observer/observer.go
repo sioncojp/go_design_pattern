209e9df852f196be02fef23ec4e7f8e87290e569
sioncojp <sion_cojp@yahoo.co.jp>
2021-11-09 19:17:49 +0900

    add observer

---
+++ observer/observer.go
@@ -0,0 +1,18 @@
+package observer
+
+type observer interface {
+	update(string)
+	getID() string
+}
+
+func main() {
+	shirtItem := newItem("Nike Shirt")
+
+	observerFirst := &customer{id: "abc@gmail.com"}
+	observerSecond := &customer{id: "xyz@gmail.com"}
+
+	shirtItem.register(observerFirst)
+	shirtItem.register(observerSecond)
+
+	shirtItem.updateAvailability()
+}
---
+++ observer/observer_test.go
@@ -0,0 +1,7 @@
+package observer
+
+import "testing"
+
+func TestObserver(t *testing.T) {
+	main()
+}
