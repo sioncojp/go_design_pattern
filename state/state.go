e83d21c510795a43511c918dd39312fff714574b
sioncojp <sion_cojp@yahoo.co.jp>
2021-11-10 18:01:35 +0900

    add state

---
+++ state/state.go
@@ -0,0 +1,56 @@
+package state
+
+import (
+	"fmt"
+	"log"
+)
+
+type state interface {
+	addItem(int) error
+	requestItem() error
+	insertMoney(money int) error
+	dispenseItem() error
+}
+
+func main() {
+	vendingMachine := newVendingMachine(1, 10)
+
+	err := vendingMachine.requestItem()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = vendingMachine.insertMoney(10)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = vendingMachine.dispenseItem()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Println()
+
+	err = vendingMachine.addItem(2)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Println()
+
+	err = vendingMachine.requestItem()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = vendingMachine.insertMoney(10)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = vendingMachine.dispenseItem()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+}
